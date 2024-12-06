52380d38deb9876ed8e52fee630808c47d6544b4
Daniel Todasca <daniel16todasca@gmail.com>
2024-12-06 06:53:57 +0200

    update day2.go with part2

--- 2024/aoc/day2.go
+++ 2024/aoc/day2.go
@@ -2,6 +2,33 @@ package main
 
 import "strings"
 
+func isSafe(report []int) bool {
+	if len(report) < 2 {
+		// all lines should have more than 2 reports (even more than 5)
+		panic("Report line is too small")
+	}
+
+	if report[0] > report[1] {
+		// reverse the list if seems like decreasing order for simplicity
+		for l, r := 0, len(report)-1; l < r; l, r = l+1, r-1 {
+			report[l], report[r] = report[r], report[l]
+		}
+	}
+
+	for i := 1; i < len(report); i++ {
+		if report[i] < report[i-1] {
+			// means it's not in increasing order
+			return false
+		}
+		diff := report[i] - report[i-1]
+		if diff < 1 || diff > 3 {
+			return false
+		}
+	}
+
+	return true
+}
+
 func main() {
 	lines, err := aocReadFile("./inputs/day2/input.txt")
 	if err != nil {
@@ -20,36 +47,23 @@ func main() {
 
 	total1 := 0
 	total2 := 0
-	for _, levels := range reports {
-		if len(levels) < 2 {
-			// all lines should have more than 2 reports (even more than 5)
-			panic("Report line is too small")
-		}
-
-		if levels[0] > levels[1] {
-			// reverse the list if seems like decreasing order for simplicity
-			for l, r := 0, len(levels)-1; l < r; l, r = l+1, r-1 {
-				levels[l], levels[r] = levels[r], levels[l]
-			}
-		}
-
-		safe := true
-		for i := 1; i < len(levels); i++ {
-			if levels[i] < levels[i-1] {
-				// means it's not in increasing order
-				safe = false
-				break
-			}
-			diff := levels[i] - levels[i-1]
-			if diff < 1 || diff > 3 {
-				safe = false
-				break
-			}
-		}
-
-		if safe {
+	for _, report := range reports {
+		if isSafe(report) {
 			total1++
+			total2++
+			continue
 		}
+
+		for idx := 0; idx < len(report); idx++ {
+			sublist := make([]int, len(report))
+			copy(sublist, report)
+			sublist = append(sublist[:idx], sublist[idx+1:]...)
+			if isSafe(sublist) {
+				total2++
+				break
+			}
+		}
+
 	}
 
 	println(total1)
